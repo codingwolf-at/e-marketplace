50a2f8a40ac8974e4f4f87f17b5285eb7af4d2f0
codingwolf-at <atameshwari9@gmail.com>
2020-11-19 11:59:36 +0530

    final

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "", "e-marketplace")
+    $con = mysqli_connect("localhost", "root", "", "e-marketplace",  )
     or die(mysqli_error($con));
     if(!isset($_SESSION)){
       session_start();

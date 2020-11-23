0be2383d9392fe2a103bab5ea7ecf194a327872f
codingwolf-at <atameshwari9@gmail.com>
2020-11-23 22:10:29 +0530

    fixed product page redirection

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 
         <!-- Website logo -->
         <div class="navbar-header">
-            <a href="index.php" class="navbar-brand">E-marketplace</a>
+            <a href="#" class="navbar-brand">E-marketplace</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mynavbar" aria-controls="mynavbar" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -18,13 +18,16 @@
                 ?>
 
                 <li class="nav-item">
+                    <a href="product.php" class="nav-link">Products</a>
+                </li>
+                <li class="nav-item">
                     <a href="cart.php" class="nav-link">Cart</a>
                 </li>
                 <li class="nav-item">
                     <a href="setting.php" class="nav-link">Setting</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">Logout</a>
+                    <a href="logout_script.php" class="nav-link">Logout</a>
                 </li>
 
                 <?php
--- signup_script.php
+++ signup_script.php
@@ -2,12 +2,12 @@
 
 require("includes/common.php");
 
-$name = $_POST['name'];
-$email = $_POST['email'];
+$name = mysqli_real_escape_string($con, $_POST['name']);
+$email = mysqli_real_escape_string($con, $_POST['email']);
 $password = $_POST['password'];
 $contact = $_POST['contact'];
-$city = $_POST['city'];
-$address = $_POST['address'];
+$city = mysqli_real_escape_string($con, $_POST['city']);
+$address = mysqli_real_escape_string($con, $_POST['address']);
 
 $signup_query = "insert into users(name, email, password, contact, city, address) 
 values ('$name', '$email', '$password', '$contact', '$contact', '$address')";
@@ -15,7 +15,10 @@ values ('$name', '$email', '$password', '$contact', '$contact', '$address')";
 $signup_submit = mysqli_query($con, $signup_query) or
 die(mysqli_error($con));
 
-echo '<script> alert("User signup successful!"); window.location="login.php"; </script>';
+$_SESSION['email'] = $email;
+$_SESSION['id'] = mysqli_insert_id($con);
+
+echo '<script> alert("User signup successful!"); window.location="product.php"; </script>';
 
 
 ?>
